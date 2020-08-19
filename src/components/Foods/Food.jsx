b2da171b5fdcec6e273e9d918143e312a4975da4
parunkov <parunkov@gmail.com>
2020-08-19 14:25:31 +0500

    добавил склонение слов

--- src/components/Foods/Food.jsx
+++ src/components/Foods/Food.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import './Food.scss';
 
-const Food = ({title}) => {
+const num2str = function (num, textForms) {
+  const n = Math.abs(num) % 100;
+  const n1 = n % 10;
+  if (n > 10 && n < 20) { return textForms[2]; }
+  if (n1 > 1 && n1 < 5) { return textForms[1]; }
+  if (n1 === 1) { return textForms[0]; }
+  return textForms[2];
+};
+
+const Food = ({title, portions, mouses, like, weight, text}) => {
 	return (
 		<div className="Food">
 			<div className="Food__card">
 				<div className="Food__description">Сказочное заморское яство</div>
 				<h2 className="Food__title">Нямушка</h2>
 				<div className="Food__subtitle">{title}</div>
+				<div className="Food__portions">{portions} {num2str(portions, ['порция', 'порции', 'порций'])}</div>
+				<div className="Food__mouses">{mouses} {num2str(mouses, ['мышь', 'мыши', 'мышей'])} в подарок</div>
+				{like && <div className="Food__like">заказчик доволен</div>}
+				<div className="Food__weight">{weight} кг</div>
 			</div>
+			<div className="Food__text">{text}</div>
 		</div>
 	)
 }
--- src/components/Foods/Foods.jsx
+++ src/components/Foods/Foods.jsx
@@ -3,7 +3,16 @@ import Food from './Food';
 
 const Foods = (props) => {
 	console.log(props);
-	const foodsElements = props.data.map(food => <Food key={food.id} title={food.title}/>);
+	const foodsElements = props.data.map(food => <Food 
+			key={food.id} 
+			title={food.title} 
+			portions={food.portions} 
+			mouses={food.mouses} 
+			like={food.like}
+			weight={food.weight}
+			text={food.text}
+			selected={food.selected}
+			disabled={food.disabled}/>);
 
 	return (
 		<div className="">
