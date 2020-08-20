c056a1d57bd8f38c979cfe5a53cd5149f608e562
parunkov <parunkov@gmail.com>
2020-08-20 08:57:22 +0500

    Updates

---
+++ precache-manifest.22a21a32cffc20bb589c52c7bf9efb96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a765398390c9b078b0131e9196ff65",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "ff08ed1402904ddb56d0",
+    "url": "/test2/static/css/main.a264679a.chunk.css"
+  },
+  {
+    "revision": "689637ec3ffc65de1c6f",
+    "url": "/test2/static/js/2.458ba85d.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/test2/static/js/2.458ba85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff08ed1402904ddb56d0",
+    "url": "/test2/static/js/main.6ebce957.chunk.js"
+  },
+  {
+    "revision": "64b17dba3a1e6860cbac",
+    "url": "/test2/static/js/runtime-main.70875b4d.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/test2/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "24f7dfdc7ad0bbdc4bbac7aa3e630ca4",
+    "url": "/test2/static/media/Pattern.24f7dfdc.png"
+  },
+  {
+    "revision": "b7467606af34213d10c667ac85094c36",
+    "url": "/test2/static/media/Photo.b7467606.webp"
+  },
+  {
+    "revision": "581a9c189d4665ba980431c002f87221",
+    "url": "/test2/static/media/Photo_opacity.581a9c18.webp"
+  }
+]);
